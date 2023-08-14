5bec8353b24880e9ba1c66521f1224bae480975a
m.klimov <mklimov2401@gmail.com>
2023-08-14 22:27:20 +0300

    Запуск проекта локально и в докере. Запуск теста.

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingWebTest.java
@@ -3,52 +3,42 @@ package guru.qa.niffler.test;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.jupiter.Spend;
-import guru.qa.niffler.jupiter.User;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import guru.qa.niffler.model.UserJson;
-import io.qameta.allure.Allure;
-import io.qameta.allure.AllureId;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static guru.qa.niffler.jupiter.User.UserType.WITH_FRIENDS;
 
-@Disabled
-public class SpendingWebTest extends BaseWebTest {
+public class SpendingWebTest {
 
     static {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1980x1024";
     }
 
-    private static final String user = "dima";
-
     @BeforeEach
-    void doLogin(@User(userType = WITH_FRIENDS) UserJson userForTest) {
+    void doLogin() {
         Selenide.open("http://127.0.0.1:3000/main");
         $("a[href*='redirect']").click();
-        $("input[name='username']").setValue(userForTest.getUsername());
-        $("input[name='password']").setValue(userForTest.getPassword());
+        $("input[name='username']").setValue("Misha");
+        $("input[name='password']").setValue("123456");
         $("button[type='submit']").click();
     }
 
+
     @Spend(
-            username = user,
+            username = "Misha",
             description = "Рыбалка на Ладоге",
             category = "Рыбалка",
             amount = 14000.00,
             currency = CurrencyValues.RUB
     )
     @Test
-    @AllureId("100")
-    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend,
-                                                  @User(userType = WITH_FRIENDS) UserJson userForTest) {
+    void spendingShouldBeDeletedAfterDeleteAction(SpendJson createdSpend) {
         $(".spendings__content tbody")
                 .$$("tr")
                 .find(text(createdSpend.getDescription()))
@@ -57,16 +47,10 @@ public class SpendingWebTest extends BaseWebTest {
                 .scrollTo()
                 .click();
 
-        Allure.step(
-                "Delete spending",
-                () -> $(byText("Delete selected")).click())
-        ;
+        $(byText("Delete selected")).click();
 
-        Allure.step(
-                "Check spendings",
-                () -> $(".spendings__content tbody")
-                        .$$("tr")
-                        .shouldHave(size(0))
-        );
+        $(".spendings__content tbody")
+                .$$("tr")
+                .shouldHave(size(0));
     }
 }
