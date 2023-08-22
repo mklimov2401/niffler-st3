2c6f0643337e8faeed36698580580cf7dda1415e
michailklimov <mklimov2401@gmail.com>
2023-08-22 12:37:09 +0300

    resolve conflict

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -4,8 +4,11 @@ import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.db.dao.AuthUserDAO;
 import guru.qa.niffler.db.dao.UserDataUserDAO;
 import guru.qa.niffler.db.model.Authority;
-import guru.qa.niffler.jupiter.Dao;
-import guru.qa.niffler.jupiter.DaoExtension;
+import guru.qa.niffler.db.model.AuthorityEntity;
+import guru.qa.niffler.db.model.UserEntity;
+import guru.qa.niffler.jupiter.annotation.Dao;
+import guru.qa.niffler.jupiter.extension.DaoExtension;
+import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,10 +28,12 @@ public class LoginTest extends BaseWebTest {
     private UserDataUserDAO userDataUserDAO;
     private UserEntity user;
 
+    private LoginPage loginPage = new LoginPage();
+
     @BeforeEach
     void createUser() {
         user = new UserEntity();
-        user.setUsername("valentin_4");
+        user.setUsername("valentin_5");
         user.setPassword("12345");
         user.setEnabled(true);
         user.setAccountNonExpired(true);
@@ -46,17 +51,13 @@ public class LoginTest extends BaseWebTest {
 
     @AfterEach
     void deleteUser() {
-        userDataUserDAO.deleteUserByUsernameInUserData(user.getUsername());
+        userDataUserDAO.deleteUserByIdInUserData(user.getId());
         authUserDAO.deleteUserById(user.getId());
+
     }
 
     @Test
     void mainPageShouldBeVisibleAfterLogin() {
-        Selenide.open("http://127.0.0.1:3000/main");
-        $("a[href*='redirect']").click();
-        $("input[name='username']").setValue(user.getUsername());
-        $("input[name='password']").setValue(user.getPassword());
-        $("button[type='submit']").click();
-        $(".main-content__section-stats").should(visible);
+        loginPage.signIn(user.getUsername(), user.getPassword());
     }
 }
